e5c9ced70bb080fda6ff15a4fbbe89915c4de5db
SamuelMunaro <sammunaro@gmail.com>
2024-03-30 23:05:37 +1030

    submission 5

--- Finder.cpp
+++ Finder.cpp
@@ -1,27 +1,17 @@
 #include "Finder.h"
 
-#include <string>
+using namespace std;
 
-std::vector<int> Finder::findSubstrings(const std::string& s1, const std::string& s2) {
-    std::vector<int> result;
+vector<int> Finder::findSubstrings(const string& s1, const string& s2) {
+    vector<int> result;
 
-    size_t prevFound = 0;
     for(size_t i = 1; i <= s2.size(); i++) {
-        size_t found = s1.find(s2.substr(0, i), prevFound);
-        if (found != std::string::npos) {
-            result.push_back(found);
-            prevFound = found + i; // Update prevFound to the end of found prefix
+        size_t found = s1.find(s2.substr(0, i));
+        if (found != string::npos) {
+        result.push_back(found);
         } else {
-            result.push_back(-1);
+        result.push_back(-1);
         }
     }
-
-    // Check if s2 is longer than s1
-    if (s2.size() > s1.size()) {
-        for (size_t i = s1.size() + 1; i <= s2.size(); i++) {
-            result.push_back(-1);
-        }
-    }
-
     return result;
 }
