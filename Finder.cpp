5ba00c77a454cc37cbc3b35b92263d87e936c1be
SamuelMunaro <sammunaro@gmail.com>
2024-03-30 22:51:48 +1030

    submission 4

--- Finder.cpp
+++ Finder.cpp
@@ -10,18 +10,17 @@ std::vector<int> Finder::findSubstrings(const std::string& s1, const std::string
         size_t found = s1.find(s2.substr(0, i), prevFound);
         if (found != std::string::npos) {
             result.push_back(found);
-            prevFound = found + 1;
+            prevFound = found + i; // Update prevFound to the end of found prefix
         } else {
-            // If any prefix is not found, the remaining won't be found either
-            for (size_t j = i; j <= s2.size(); j++)
-                result.push_back(-1);
-            break;
+            result.push_back(-1);
         }
     }
 
-    // If some substrings are still needed to be added
-    while (result.size() < s2.size()) {
-        result.push_back(-1);
+    // Check if s2 is longer than s1
+    if (s2.size() > s1.size()) {
+        for (size_t i = s1.size() + 1; i <= s2.size(); i++) {
+            result.push_back(-1);
+        }
     }
 
     return result;
